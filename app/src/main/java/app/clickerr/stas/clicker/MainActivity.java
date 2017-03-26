73ab4f4abf89b269f7a298c1e74508e5ca34272c
Otrotskyi <anton.otrotskyi@gmail.com>
2017-03-27 02:23:01 +0300

    Test commit

--- app/src/main/java/app/clickerr/stas/clicker/MainActivity.java
+++ app/src/main/java/app/clickerr/stas/clicker/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnHard;
 
     private int g;
-    private int test111666666666666666666666666;
+    private int abcd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
