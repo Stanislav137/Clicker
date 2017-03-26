6d725f666c0737f280ce1e73266bae71182fc322
Stason Stas <semenetsbeph@gmail.com>
2017-03-26 21:53:31 +0200

    first commit

---
+++ app/src/main/java/app/clickerr/stas/clicker/GameActivity.java
@@ -0,0 +1,42 @@
+package app.clickerr.stas.clicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GameActivity extends AppCompatActivity {
+
+    private int difficulty;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+    }
+
+    public void test(View view) {
+        Bundle bundle = getIntent().getExtras();
+        this.difficulty = bundle.getInt("difficulty");
+        btn1.setText("" + difficulty);
+    }
+}
---
+++ app/src/main/java/app/clickerr/stas/clicker/MainActivity.java
@@ -0,0 +1,43 @@
+package app.clickerr.stas.clicker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnEasy;
+    private Button btnMedium;
+    private Button btnHard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEasy = (Button) findViewById(R.id.btnEasy);
+        btnMedium = (Button) findViewById(R.id.btnMedium);
+        btnHard = (Button) findViewById(R.id.btnHard);
+    }
+
+    public void startEasy(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("difficulty", 1);
+        startActivity(intent);
+    }
+
+    public void startMedium(View view) {
+
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("difficulty", 2);
+        startActivity(intent);
+    }
+
+    public void startHard(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("difficulty", 3);
+        startActivity(intent);
+    }
+}
